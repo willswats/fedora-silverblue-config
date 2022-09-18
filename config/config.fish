ccd34eb2cedbe7ab0061c6235b38752844aa21f6
willswats <williamstuwatson@gmail.com>
2022-09-18 17:49:52 +0100

    Add more to config.fish

--- config/config.fish
+++ config/config.fish
@@ -4,8 +4,28 @@ end
 
 set -g fish_greeting
 
+# flatpak
+
 alias flatdel="flatpak uninstall --delete-data"
+
+# distrobox
 
 alias main="distrobox enter main -- fish"
 
+# mpv
+
 alias mpv="flatpak run io.mpv.Mpv"
+
+function playaudio
+    clear
+    flatpak run io.mpv.Mpv --no-audio-display --volume=50 $argv
+end
+
+function playaudioshuffle
+    clear
+    flatpak run io.mpv.Mpv --no-audio-display --shuffle --volume=50 $argv
+end
+
+# youtube-dl
+
+alias ytdlmp3="youtube-dl --rm-cache-dir --add-metadata --embed-thumbnail --extract-audio --audio-format mp3"
