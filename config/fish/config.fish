880d7541b9c26482b6f4f6dd7f5ce10c46a62e8c
willswats <williamstuwatson@gmail.com>
2022-09-21 00:29:25 +0100

    Update mpv.md and fish.config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -12,21 +12,6 @@ alias flatdel="flatpak uninstall --delete-data"
 
 alias main="distrobox enter main -- fish"
 
-# mpv
-
-alias mpv="flatpak run io.mpv.Mpv"
-
-function mpvaudio
-    clear
-    flatpak run io.mpv.Mpv --volume=50 $argv
-end
-
-# youtube-dl
-
-alias ytdlmp3="youtube-dl --rm-cache-dir --audio-quality 0 --add-metadata --embed-thumbnail --extract-audio --audio-format mp3 -o '%(title)s.%(ext)s'"
-
-alias ytdlmusic="youtube-dl --rm-cache-dir --audio-quality 0 --add-metadata --embed-thumbnail --extract-audio --audio-format mp3 -o '%(artist)s - %(track)s.%(ext)s'"
-
 # wireguard-tools
 
 alias vpnon="wg-quick up gb-lon-wg-101"
--- guides/setup/applications/command-line/mpv.md
+++ guides/setup/applications/command-line/mpv.md
@@ -1,5 +1,8 @@
 # mpv
 
-1. Download [playlistmanager.lua](https://github.com/jonniek/mpv-playlistmanager) and [youtube-download.lua](https://github.com/cvzi/mpv-youtube-download).
+1. Download:
+   - [playlistmanager.lua](https://github.com/jonniek/mpv-playlistmanager)
+   - [youtube-download.lua](https://github.com/cvzi/mpv-youtube-download)
 2. Add the files to `~/.var/app/io.mpv.Mpv/config/mpv/scripts`.
 3. Add the files in `config/mpv` to `~/.var/app/io.mpv.Mpv/config/mpv/script-opts` (create the directory if it does not exist).
+4. Add the `downloads` folder as a bookmark (create the directory if it does not exist).
